cfcdf404110dc6caa71f65e8c522bc0fce22a08e
Michael McCarthy <mike@pixelonezero.com>
2020-11-18 21:18:46 -0800

    Add files for event loop visualization

--- js/event-loop.js
+++ js/event-loop.js
@@ -1,5 +1,29 @@
+let result;
 
+function first() {
+  result.innerHTML += ' first ';
+}
 
-window.addEventListener('DOMContentLoaded', function() {
+function second() {
+  setTimeout(function() {
+    result.innerHTML += ' second ';
+  }, 500);
+}
 
-});
+function third() {
+  result.innerHTML += ' third ';
+}
+
+function fourth() {
+  result.innerHTML += ' fourth ';
+}
+
+function init() {
+  result = document.getElementById('result');
+  first();
+  second();
+  third();
+  fourth();
+}
+
+window.addEventListener('DOMContentLoaded', init);
