fd1c633b0088466efb4b55762216153f18e97df1
Michael McCarthy <mike@pixelonezero.com>
2020-11-30 20:45:04 -0800

    Print each performance entry with details

--- js/performance.js
+++ js/performance.js
@@ -1,6 +1,57 @@
+function print_PerformanceEntries() {
+  // Use getEntries() to get a list of all performance entries
+  var p = performance.getEntries();
+  for (var i=0; i < p.length; i++) {
+    console.log("PerformanceEntry[" + i + "]");
+    print_PerformanceEntry(p[i]);
+		log_sizes(p[i]);
+  }
+}
+function print_PerformanceEntry(perfEntry) {
+  var properties = ["name",
+                    "entryType",
+                    "startTime",
+                    "duration"];
+
+  for (var i=0; i < properties.length; i++) {
+    // Check each property
+    var supported = properties[i] in perfEntry;
+    if (supported) {
+      var value = perfEntry[properties[i]];
+      console.log(`${properties[i]} = ${value}`);
+    }
+  }
+}
+
+function log_sizes(perfEntry){
+  // Check for support of the PerformanceEntry.*size properties and print their values
+  // if supported.
+  if ("decodedBodySize" in perfEntry)
+    console.log("decodedBodySize = " + perfEntry.decodedBodySize);
+  else
+    console.log("decodedBodySize = NOT supported");
+
+  if ("encodedBodySize" in perfEntry)
+    console.log("encodedBodySize = " + perfEntry.encodedBodySize);
+  else
+    console.log("encodedBodySize = NOT supported");
+
+  if ("transferSize" in perfEntry)
+    console.log("transferSize = " + perfEntry.transferSize);
+  else
+    console.log("transferSize = NOT supported");
+}
+function check_PerformanceEntries() {
+  // Use getEntriesByType() to just get the "resource" events
+  var p = performance.getEntriesByType("resource");
+  for (var i=0; i < p.length; i++) {
+    log_sizes(p[i]);
+  }
+}
+
 
 function init() {
-
+		print_PerformanceEntries();
 }
 
 window.addEventListener('DOMContentLoaded', init);
