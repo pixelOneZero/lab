8ec0dcccc44b871c86838696de2bfdba18842d3e
Michael McCarthy <mike@pixelonezero.com>
2020-11-30 21:13:02 -0800

    Remove extra logging and add image to img directory

--- js/performance.js
+++ js/performance.js
@@ -1,54 +1,25 @@
 function print_PerformanceEntries() {
-  // Use getEntries() to get a list of all performance entries
   var p = performance.getEntries();
   for (var i=0; i < p.length; i++) {
-    console.log("PerformanceEntry[" + i + "]");
     print_PerformanceEntry(p[i]);
 		log_sizes(p[i]);
   }
 }
 function print_PerformanceEntry(perfEntry) {
-  var properties = ["name",
-                    "entryType",
-                    "startTime",
-                    "duration"];
+    if ("name" in perfEntry) {
+      console.log(`name = ${perfEntry['name']}`);
+    }
+}
 
-  for (var i=0; i < properties.length; i++) {
-    // Check each property
-    var supported = properties[i] in perfEntry;
-    if (supported) {
+function log_sizes(perfEntry) {
+	var properties = ["decodedBodySize", "encodedBodySize", "transferSize"];
+	for (var i=0; i < properties.length; i++) {
+    if (properties[i] in perfEntry) {
       var value = perfEntry[properties[i]];
       console.log(`${properties[i]} = ${value}`);
     }
   }
 }
-
-function log_sizes(perfEntry){
-  // Check for support of the PerformanceEntry.*size properties and print their values
-  // if supported.
-  if ("decodedBodySize" in perfEntry)
-    console.log("decodedBodySize = " + perfEntry.decodedBodySize);
-  else
-    console.log("decodedBodySize = NOT supported");
-
-  if ("encodedBodySize" in perfEntry)
-    console.log("encodedBodySize = " + perfEntry.encodedBodySize);
-  else
-    console.log("encodedBodySize = NOT supported");
-
-  if ("transferSize" in perfEntry)
-    console.log("transferSize = " + perfEntry.transferSize);
-  else
-    console.log("transferSize = NOT supported");
-}
-function check_PerformanceEntries() {
-  // Use getEntriesByType() to just get the "resource" events
-  var p = performance.getEntriesByType("resource");
-  for (var i=0; i < p.length; i++) {
-    log_sizes(p[i]);
-  }
-}
-
 
 function init() {
 		print_PerformanceEntries();
